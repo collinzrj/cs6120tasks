78419938a54e538dbfc22e159cc699f16ef61bf0
Collin Zhang <collinzrj@gmail.com>
2023-08-31 13:17:49 -0400

    task2 tool

---
+++ task2/tool.py
@@ -0,0 +1,12 @@
+import json, sys
+
+def remove_print(file):
+    with open(file, 'r') as f:
+        code = json.load(f)
+    for fn in code['functions']:
+        fn['instrs'] = list(filter(lambda x: x['op'] != 'print', fn['instrs']))
+    print(json.dumps(code,  indent=2))
+
+
+if __name__ == '__main__':
+    remove_print(sys.argv[1])

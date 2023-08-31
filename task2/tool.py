14fcc108a405602ccf781a2df5870e42ecea33c0
collinzrj <collinzrj@gmail.com>
2023-08-31 19:39:39 -0400

    add test

--- task2/tool.py
+++ task2/tool.py
@@ -6,9 +6,10 @@ def remove_print(file):
     with open(file, 'r') as f:
         code = json.load(f)
     for fn in code['functions']:
-        fn['instrs'] = list(filter(lambda x: x['op'] != 'print', fn['instrs']))
+        fn['instrs'] = list(filter(lambda x: 'op' not in x or x['op'] != 'print', fn['instrs']))
     print(json.dumps(code,  indent=2))
 
 
 if __name__ == '__main__':
+    print(sys.argv[1])
     remove_print(sys.argv[1])

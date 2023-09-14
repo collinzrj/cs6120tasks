248fb6f17bb4ab4773dfc13ee432d382b1796f27
collinzrj <collinzrj@gmail.com>
2023-09-14 18:40:12 -0400

    basic finish

--- task4/dataflow_analysis.py
+++ task4/dataflow_analysis.py
@@ -3,42 +3,49 @@
 import uuid, json, sys
 from typing import Dict, Any, List
 
+
 def build_cfg(instrs):
-    """build cfg for global analysis in a function"""
-    blocks = {}
-    block_parents = {}
-    block_children = {}
+    blocks = []
     ## give a unique name if no label
-    name_block = (uuid.uuid4().hex, [])
+    current_block = []
     for inst in instrs:
         if 'label' in inst.keys():
-            if len(name_block[1]) > 0:
-                blocks[name_block[0]] = name_block[1]
-            # TODO: check this
-            last_block_name = name_block[0]
-            name_block = (inst['label'], [inst])
-            block_children.setdefault(last_block_name, set()).add(name_block[0])
-        elif inst['op'] in ['jmp', 'br', 'call', 'ret']:
-            name_block[1].append(inst)
-            blocks[name_block[0]] = name_block[1]
-            if 'labels' in inst:
-                block_children.setdefault(name_block[0], set()).union(set(inst['labels']))
-            last_block_name = name_block[0]
-            name_block = (uuid.uuid4().hex, [])
-            block_children.setdefault(last_block_name, set()).add(name_block[0])
+            if len(current_block) > 0:
+                blocks.append(current_block)
+            current_block = [inst]
+        elif inst['op'] in ['jmp', 'br', 'ret']:
+            current_block.append(inst)
+            if len(current_block) > 0:
+                blocks.append(current_block)
+            current_block = []
         else:
-            name_block[1].append(inst)
-    ## append last block
-    blocks[name_block[0]] = name_block[1]
+            current_block.append(inst)
+    if len(current_block) > 0:
+        blocks.append(current_block)
+    # name the blocks
+    name_block_list = []
+    for block in blocks:
+        if 'label' in block[0]:
+            name_block_list.append((block[0]['label'], block))
+        else:
+            name_block_list.append((uuid.uuid4().hex, block))
+    # build the cfg
+    block_children = {}
+    for idx, (name, block) in enumerate(name_block_list):
+        if 'op' in block[-1] and block[-1]['op'] in ['jmp', 'br', 'ret']:
+            if 'labels' in block[-1]:
+                block_children[name] = block[-1]['labels']
+        else:
+            # TODO: is this correct?
+            if idx + 1 < len(name_block_list):
+                block_children[name] = [name_block_list[idx + 1][0]]
     ## construct parents block
+    block_parents = {}
     for (parent_name, children) in block_children.items():
         for child_name in children:
-            if child_name in block_parents:
-                block_parents[child_name].append(parent_name)
-            else:
-                block_parents[child_name] = [parent_name]
+            block_parents.setdefault(child_name, []).append(parent_name)
     cfg = {}
-    for (name, instrs) in blocks.items():
+    for (name, block) in name_block_list:
         if name not in block_parents:
             block_parents[name] = []
         if name not in block_children:
@@ -46,9 +53,10 @@ def build_cfg(instrs):
         cfg[name] = {}
         cfg[name]['parents'] = block_parents[name]
         cfg[name]['children'] = block_children[name]
-        cfg[name]['content'] = instrs
+        cfg[name]['content'] = block
+    # for (key, val) in cfg.items():
+    #     print(key, val)
     return cfg
-
 
 class ReachabilityAnalyzer:
     """Analyzer that performs reachability analysis"""
@@ -65,9 +73,7 @@ class ReachabilityAnalyzer:
             if label not in self.block_out_dict:
                 self.block_out_dict[label] = {}
             for (var, definitions) in self.block_out_dict[label].items():
-                if var not in current_block_out:
-                    current_block_out[var] = []
-                current_block_out[var] += definitions
+                current_block_out[var] = current_block_out.setdefault(var, set()).union(definitions)
         return current_block_out
 
     def transfer(self, block, block_in):
@@ -75,48 +81,39 @@ class ReachabilityAnalyzer:
         for instr in block:
             if 'dest' in instr:
                 # block_in[instr['dest']] = [instr]
-                block_in[instr['dest']] = ['placeholder']
+                block_in[instr['dest']] = {uuid.uuid4().hex}
         return block_in
 
 
 # TODO: check if this is correct
 def worklist_algo(cfg, analyzer: ReachabilityAnalyzer):
     """worklist algorithm"""
-    worklist: List[str] = list(cfg.keys())
-    n = 0
-    while len(worklist) > 1:
-        n += 1
-        if n > 100:
-            break
+    worklist = set(cfg.keys())
+    while len(worklist) > 0:
         print(worklist)
         b = worklist.pop()
         analyzer.block_in_dict[b] = analyzer.merge(cfg[b]['parents'])
-        if b not in analyzer.block_out_dict:
-            analyzer.block_out_dict[b] = {}
-        old_block_out_dict = analyzer.block_out_dict[b]
+        old_block_out_dict = analyzer.block_out_dict.setdefault(b, {})
         analyzer.block_out_dict[b] = analyzer.transfer(cfg[b]['content'], analyzer.block_in_dict[b].copy())
         if analyzer.block_out_dict[b] != old_block_out_dict:
-            worklist.extend(cfg[b]['children'])
+            worklist.union(cfg[b]['children'])
 
 
 def dataflow_analysis(fn):
     """entry point for dataflow analysis"""
     cfg = build_cfg(fn['instrs'])
-    print("before cfg")
-    for key in cfg:
-        print(key, cfg[key])
-    print("after cfg")
     analyzer = ReachabilityAnalyzer()
     worklist_algo(cfg, analyzer)
     return analyzer.block_in_dict, analyzer.block_out_dict
 
 if __name__ == '__main__':
-    file = '/Users/collin/Documents/Projects/cs6120tasks/task3/test/rect.json'
+    file = '/Users/collin/Documents/Projects/cs6120tasks/task3/test/gcd.json'
     # file = sys.argv[1]
     with open(file, 'r') as f:
         code = json.load(f)
         for fn in code['functions']:
             output = dataflow_analysis(fn)
-            print("will print json")
-            print(json.dumps(output, indent=2))
-
+            print("####INPUT####")
+            print(output[0])
+            print("####OUTPUT####")
+            print(output[1])
